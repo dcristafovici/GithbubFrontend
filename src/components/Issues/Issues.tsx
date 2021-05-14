c9efae2492301628799b33bba78ab47bac9c446f
denibudeyko <denbudeyko@gmail.com>
2021-05-14 10:56:27 +0300

    basic functionaly

--- src/components/Issues/Issues.tsx
+++ src/components/Issues/Issues.tsx
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react'
 import IssuesItem from './IssuesItem'
 import "./Issues.sass"
+import { useTypeSelector } from '../../types/useTypeSelector'
 
 const Issues:React.FC = () => {
   
 
+  const {issues, errors, loading} = useTypeSelector(state => state.issuesReducer)
+  console.log(issues) 
 
   return(
     <section className="issues">
       <div className="container">
         <div className="issues-items">
-
-          <IssuesItem />
-          <IssuesItem />
-          <IssuesItem />
-          <IssuesItem />
-     
-
+          {issues.length ? issues.map((item:any, index:any) => {
+            console.log(item)
+            return(
+              <IssuesItem user={item?.user} title={item.title} date={item.created_at} number={item.number} key={item.number} />
+            )
+          }) : ""}
 
         </div>
       </div>
